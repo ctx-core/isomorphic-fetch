c19ab8830556a4755505eeba8c6475980f23e4a1
Brian Takita <brian.takita@gmail.com>
2021-02-23 18:17:31 -0500

    update dependencies

--- src/browser.d.ts
+++ src/browser.d.ts
@@ -1,12 +1,12 @@
 import 'whatwg-fetch';
 export declare const fetch: ((input: RequestInfo, init?: RequestInit | undefined) => Promise<Response>) & typeof globalThis.fetch, Headers: {
-    new (init?: string[][] | Headers | Record<string, string> | undefined): Headers;
+    new (init?: HeadersInit | undefined): Headers;
     prototype: Headers;
 }, Request: {
     new (input: RequestInfo, init?: RequestInit | undefined): Request;
     prototype: Request;
 }, Response: {
-    new (body?: string | ArrayBuffer | ArrayBufferView | Blob | FormData | URLSearchParams | ReadableStream<Uint8Array> | null | undefined, init?: ResponseInit | undefined): Response;
+    new (body?: BodyInit | null | undefined, init?: ResponseInit | undefined): Response;
     prototype: Response;
     error(): Response;
     redirect(url: string, status?: number | undefined): Response;
