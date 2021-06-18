21a2117629baf470374acab98d5da45a865ec170
Brian Takita <brian.takita@gmail.com>
2021-06-18 04:09:33 -0400

    rm src/*.js

--- src/index.js
+++
@@ -1,9 +0,0 @@
-import real_fetch from 'node-fetch';
-export { Headers, Request, Response } from 'node-fetch';
-export function fetch(url, options) {
-    if (/^\/\//.test(url)) {
-        url = 'https:' + url;
-    }
-    return real_fetch.call(this, url, options);
-}
-//# sourceMappingURL=src/index.js.map
